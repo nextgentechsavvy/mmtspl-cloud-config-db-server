05e382e20532149f6d5923778f89b4fd7042ad2b
mithunmajumdar <71630267+mithunmajumdar@users.noreply.github.com>
2024-07-13 18:30:14 +0530

    Base commit

---
+++ src/main/java/com/mmtspl/cloudconfigdbserver/MMTSPLCloudConfigDBServerApplication.java
@@ -0,0 +1,17 @@
+package com.mmtspl.cloudconfigdbserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+//@EnableDiscoveryClient
+@SpringBootApplication
+public class MMTSPLCloudConfigDBServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MMTSPLCloudConfigDBServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mmtspl/cloudconfigdbserver/MMTSPLCloudConfigDBServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmtspl.cloudconfigdbserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MMTSPLCloudConfigDBServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
